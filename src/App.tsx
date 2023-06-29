67e3a53ebc35311f1af974df2cbd66c9f5b7b224
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-06-30 05:39:01 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <h1 className="text-8xl font-bold">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
