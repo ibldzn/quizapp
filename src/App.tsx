dcaed83ddebc8d0cdf61fd688ef3539beb654509
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-07-03 01:06:16 +0700

    refactor: use HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { QuestionsProvider } from "./context/Questions";
 import { Categories } from "./pages/Categories";
 import { Home } from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Quiz } from "./pages/Quiz";
 import { About } from "./pages/About";
 import { Results } from "./pages/Results";
